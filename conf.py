830304d92c197b51b6544889a657f107315cf3a0
Dave Barry <david.barry@crick.ac.uk>
2021-12-17 16:42:57 +0000

    Update conf.py

--- conf.py
+++ conf.py
@@ -49,8 +49,8 @@ author = 'GIANI docs authors'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -58,9 +58,9 @@ html_theme = 'alabaster'
 #html_static_path = ['_static']
 
 
-#html_theme_options = {
-#    'style_nav_header_background': '#343131'
-#}
+html_theme_options = {
+    'style_nav_header_background': '#343131'
+}
 
 #html_css_files = [
 #    'css/custom.css',
